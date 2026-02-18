318561c89757f5e04e7445c1cfa1499e02a17617
Misha-Kamyshev <kam628326@gmail.com>
2026-02-18 15:20:36 +1000

    Покрашен текст во фрагментах входа и регистрации

--- app/src/main/java/com/example/itemly/ui/authorization/AuthFragment.kt
+++ app/src/main/java/com/example/itemly/ui/authorization/AuthFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.itemly.R
+import com.example.itemly.data.model.DataSpanConfig
 import com.example.itemly.databinding.FragmentAuthBinding
 import com.example.itemly.ui.main.MainActivity
+import com.example.itemly.utils.buildColoredSpannable
 
 class AuthFragment : Fragment() {
     private var _binding: FragmentAuthBinding? = null
@@ -27,6 +31,8 @@ class AuthFragment : Fragment() {
         binding.buttonSignIn.setOnClickListener { onClickButtonSignIn() }
         binding.hrefSignUp.setOnClickListener { onClickSingUp() }
         binding.errorSignInTextView.visibility = View.GONE
+
+        paintText()
     }
 
     private fun onClickButtonSignIn() {
@@ -41,5 +47,24 @@ class AuthFragment : Fragment() {
 
     private fun onClickSingUp() {
         (requireActivity() as MainActivity).openFragment(RegFragment())
+    }
+
+    private fun paintText() {
+        val fullText: String = ContextCompat.getString(binding.root.context, R.string.href_sign_up)
+        val highlightText = "Зарегистрируйтесь"
+        val start = fullText.indexOf(highlightText)
+
+        val spannable = buildColoredSpannable(
+            context = binding.root.context,
+            fullText = fullText,
+            spans = listOf(
+                DataSpanConfig(
+                    start = start,
+                    end = start + highlightText.length,
+                    colorRes = R.color.main_color
+                )
+            )
+        )
+        binding.hrefSignUp.text = spannable
     }
 }
--- app/src/main/java/com/example/itemly/ui/authorization/RegFragment.kt
+++ app/src/main/java/com/example/itemly/ui/authorization/RegFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.itemly.R
+import com.example.itemly.data.model.DataSpanConfig
 import com.example.itemly.databinding.FragmentRegBinding
 import com.example.itemly.ui.main.MainActivity
+import com.example.itemly.utils.buildColoredSpannable
 
 class RegFragment : Fragment() {
     private var _binding: FragmentRegBinding? = null
@@ -27,6 +31,8 @@ class RegFragment : Fragment() {
         binding.buttonSignUp.setOnClickListener { onClickButtonSingUp() }
         binding.hrefSignIn.setOnClickListener { onClickSignIn() }
         binding.errorSignUpTextView.visibility = View.GONE
+
+        paintText()
     }
 
     private fun onClickButtonSingUp() {
@@ -47,5 +53,24 @@ class RegFragment : Fragment() {
 
     private fun onClickSignIn() {
         (requireActivity() as MainActivity).openFragment(AuthFragment())
+    }
+
+    private fun paintText() {
+        val fullText: String = ContextCompat.getString(binding.root.context, R.string.href_sign_in)
+        val highlightText = "Войти"
+        val start = fullText.indexOf(highlightText)
+
+        val spannable = buildColoredSpannable(
+            context = binding.root.context,
+            fullText = fullText,
+            spans = listOf(
+                DataSpanConfig(
+                    start = start,
+                    end = start + highlightText.length,
+                    colorRes = R.color.main_color
+                )
+            )
+        )
+        binding.hrefSignIn.text = spannable
     }
 }
