41a400b4e77def09ca91376dac1d124d6e4466ef
Misha-Kamyshev <kam628326@gmail.com>
2026-02-24 12:14:37 +1000

    Удалены неправильные фрагменты

--- app/src/main/java/com/example/itemly/ui/buy_list/BuyFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.itemly.ui.buy_list
-
-import androidx.fragment.app.Fragment
-
-class BuyFragment : Fragment() {
-}
--- app/src/main/java/com/example/itemly/ui/inventory/InventoryFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.example.itemly.ui.inventory
-
-import androidx.fragment.app.Fragment
-
-class InventoryFragment : Fragment() {
-}
