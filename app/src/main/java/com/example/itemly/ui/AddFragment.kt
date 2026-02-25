39e29abdf1191e0740c4fb47971a52a6d4ab731f
Misha-Kamyshev <kam628326@gmail.com>
2026-02-25 10:49:13 +1000

    Создан фрагмент "AddFragment"

---
+++ app/src/main/java/com/example/itemly/ui/AddFragment.kt
@@ -0,0 +1,30 @@
+package com.example.itemly.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.example.itemly.databinding.FragmentAddBinding
+import com.example.itemly.ui.main.MainActivity
+
+class AddFragment : Fragment() {
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.closeFragmentAdd.setOnClickListener { (activity as? MainActivity)?.closeAddFragment() }
+    }
+}
