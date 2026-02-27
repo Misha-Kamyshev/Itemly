13f08f3450fd3fe3bbcccd05e2015a4f9608af59
Misha-Kamyshev <kam628326@gmail.com>
2026-02-27 16:56:24 +1000

    Подключен импорт

--- app/src/main/java/com/example/itemly/ui/add/AddFragment.kt
+++ app/src/main/java/com/example/itemly/ui/add/AddFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.itemly.databinding.FragmentAddBinding
+import com.example.itemly.ui.addPhoto.AddPhotoFragment
 import com.example.itemly.ui.main.MainActivity
 
 class AddFragment : Fragment() {
